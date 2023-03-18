d7a96c36bb823fdcbd3ed1c7da4612fbb8ea3596
Nury Amanmadov <amanmadov@gmail.com>
2023-03-17 22:09:52 -0700

    create two more component fix responsiveness issue

---
+++ force-app/main/default/lwc/customThemeLayout/customThemeLayout.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * @slot c-right-section This is the c-right-section slot
+ * @slot c-left-section This is the c-left-section slot
+ * @slot default This is the default slot
+ */
+
+export default class CustomThemeLayout extends LightningElement {
+
+}
